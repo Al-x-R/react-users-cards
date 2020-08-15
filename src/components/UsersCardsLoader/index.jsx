ea4e8d61e31fdc924dc8171088cc67cb59e99f74
Alexander Roshchin <alexander.roshchin85@gmail.com>
2020-08-16 00:42:44 +0300

    Add loading of the following cards

---
+++ src/components/UsersCardsLoader/index.jsx
@@ -0,0 +1,79 @@
+import React, {Component} from "react"
+import {getUsers} from "../../api"
+import UserCard from './../UserCard'
+import styles from './UsersCardsLoader.module.scss'
+import Spinner from './../Spinner'
+
+class DataLoader extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            isFetching: true,
+            users: [],
+            error: null,
+            currentPage: 1,
+        }
+    }
+
+    loadUsers = () => {
+        const {currentPage, users} = this.state
+        getUsers({
+            page: currentPage,
+        })
+            .then(data => {
+                this.setState({
+                    page: currentPage,
+                    users: users.concat(data.results),
+                    isFetching: false,
+                })
+            })
+            .catch(error => {
+                this.setState({
+                    error,
+                    isFetching: false,
+                })
+            })
+    }
+
+    componentDidMount() {
+        this.loadUsers()
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        const {currentPage,} = this.state
+        if (prevState.currentPage !== currentPage) {
+            this.loadUsers()
+        }
+    }
+
+    next = () => {
+        const {currentPage} = this.state
+        this.setState({
+            currentPage: currentPage + 1,
+        })
+    }
+
+    render() {
+        const {isFetching, users, error} = this.state
+
+        if (error) {
+            return <div>Error</div>
+        }
+        if (isFetching) {
+            return <Spinner/>
+        }
+        return (
+            <div>
+                <article className={styles.usersCards}>
+                    {users.map(user => (
+                        <UserCard key={user.email} user={user}/>
+                    ))}
+                </article>
+                <button onClick={this.next}>Download more</button>
+            </div>
+        )
+    }
+}
+
+export default DataLoader
